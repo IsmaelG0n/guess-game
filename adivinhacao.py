16610de7ca833578beb88c60da266369a9b7ac4e
Ismael_goncalves <ismaelgsilva22@gmail.com>
2026-09-17 18:07:45 -0300

    Versão inicial do jogo de adivinhacao

---
+++ adivinhacao.py
@@ -0,0 +1,20 @@
+import random
+
+secret = random.randint (1, 100)
+guesses = 0
+
+# Sistema para adivinhar o número
+while True:
+
+    guess = int(input("Guess a number (1-100): "))
+    guesses += 1
+
+    if guess < secret:
+        print("Too low")
+
+    elif guess > secret:
+        print("Too high")
+
+    else:
+        print(f"Got it in {guesses} guesses")
+        break
