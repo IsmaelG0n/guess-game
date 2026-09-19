2dc24aeab77c25f66484347e7085abf4e31756e6
Ismael_goncalves <ismaelgsilva22@gmail.com>
2026-09-19 15:51:56 -0300

    Adicionei niveis de dificuldade

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,13 +1,34 @@
 import random
 
-secret = random.randint (1, 100)
-guesses = 0
+#adicionar a escolha de dificuldade antes de iniciar o game.
+difficulty = input ("Easy, Medium or Hard?").lower()
+
+if difficulty == "easy":
+        max_number = 50
+
+elif difficulty == "hard":
+        max_number = 1000
+
+else:
+     max_number = 100
+
+secret = random.randint (1, max_number)
+guesses = 0 # numero de tentativas
 max_attempts = 7
 
-# Sistema para adivinhar o número
+# Looping para adivinhar o número
 while guesses < max_attempts:
 
-    guess = int(input("Guess a number (1-100): "))
+    guess = int(input(f"Guess a number (1-{max_number}): "))
+
+    if guess < 1 or guess > max_number:
+         print (
+              f"This number is not part of the game. "
+              f"Try again! You still have "
+              f"{max_attempts - guesses} attempts left."
+         )
+         continue
+
     guesses += 1
 
     if guess < secret:
